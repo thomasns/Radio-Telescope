bb6a3bb4b2d907ee89c938c0ecc50ec6eb333ebc
thomasns <ki4hbd.nathan@gmail.com>
2019-01-23 00:55:54 -0500

    Started adding serial communication to the motor controller. Known bugs: only sends the first move command currently. Ocassionally sents a malformed command to the arduino, causing a reset

--- Frontend/controller.py
+++ Frontend/controller.py
@@ -16,13 +16,19 @@ class RotorController:
 		self.EL_DEGREES_PER_TICK = 1/constant.EL_TICKS_PER_DEGREE
 		self.directionAZ = 0 # undefined direction
 		self.directionEL = 0 # undefined direction
-		self.currentAZ = 999 # undefined Azmith
-		self.currentEL = 999 # undefined Elevation
+		self.currentAZ = 0 # undefined Azmith
+		self.currentEL = 0 # undefined Elevation
 		self.statusAZ = constant.MotionStatus[3]
 		self.statusEL = constant.MotionStatus[3]
 		self.targetAZ = 999; #undefined
-		self.targetEL = 999; #undefined
+		self.targetEL = 999; #undefined	
+		self.ser = serial.Serial('/dev/ttyACM0',timeout=5)
+		line = self.ser.readline() + self.ser.readline() + self.ser.readline();
 
+		self.listener = threading.Thread(target=self.checkFeedback, args=())
+		self.e = threading.Event()
+		self.listener.start()
+		print line
 
 	def moveHome(self):
 		#send command to move here
@@ -46,46 +52,71 @@ class RotorController:
                     raise Exception('DEC should be between -180 and 180. DEC was: {}'.format(DEC))
 
 		#set up EL motion
-		steps = (RA - self.currentEL) / self.EL_DEGREES_PER_TICK 
+		steps = int((RA - self.currentEL) / self.EL_DEGREES_PER_TICK)
 		if(steps < 0):
-			neg = True
+			dir = 'p'
 		else:
-			neg = False
+			dir = 'n'
 		#send EL  move commend
-
+		self.sendCommand('move EL ' + dir +  ' ' + str(steps/1))
 		#set up AZ motion
-		steps = (DEC - self.currentAZ) / self.AZ_DEGREES_PER_TICK 
+		steps = int((DEC - self.currentAZ) / self.AZ_DEGREES_PER_TICK)
 		if(steps < 0):
-			neg = True
+			dir = 'p'
 		else:
-			neg = False
-		#send AZ  move commend
+			dir = 'n'
+		self.sendCommand('move AZ ' + dir +  ' ' + str(steps/1))
 		self.statusAZ = constant.MotionStatus[1]
 		self.statusEL = constant.MotionStatus[1]
+		while self.ser.out_waiting > 0:
+			pass
+		self.e.set()
 
         #parses a command from the rotorController
-	def parseCommand(self, line):
+	def parseFeedback(self, line):
+		command = line.split(':')[0]
+		params = line.split(':')[1]
 
-            command = line.split(':')[0]
-            params = line.split(':')[1]
-    
-            if command == 'STEP':
-                if params == 'AZ':
-                    self.currentAZ += self.AZ_DEGREES_PER_TICK
-                elif params == 'EL':
-                    self.currentEL += self.AZ_DEGREES_PER_TICK
-                print('Moved 1 pules on the ' + params + ' axis')
+		if command == 'STEPPED':
+			if params == 'AZ':
+			    self.currentAZ += self.AZ_DEGREES_PER_TICK
+			elif params == 'EL':
+			    self.currentEL += self.AZ_DEGREES_PER_TICK
+			print('Moved 1 pules on the ' + params + ' axis')
+			#do something
+		elif command == 'STOP':
+			print( 'Finished travel on the ' + params + ' axis')
+			if params == 'AZ':
+				self.statusAZ = constant.MotionStatus[0]
+			elif params == 'EL':
+				self.statusEL = constant.MotionStatus[0]
+			if self.statusAZ == constant.MotionStatus[0] and self.statusEL == constant.MotionStatus[0]:
+				self.e.clear()
+	
+			
                 #do something
-            elif command == 'STOP':
-                print( 'Finished travel on the ' + params + ' axis')
-                #do something
-            elif command == 'HOME':
-                print('Arrived home on the ' + params + ' axis')
-                self.currentAZ = 0
-                self.currentEl = 0
-                #do something
-            elif command == 'WARN':
-                print('Reveived a ' + params.split(' ')[0] + ' warning on the ' + params.split(' ')[1] + ' axis')
-                #do something:
+		elif command == 'HOME':
+			print('Arrived home on the ' + params + ' axis')
+			self.currentAZ = 0
+			self.currentEl = 0
+			#do something
+		elif command == 'WARN':
+			print('Reveived a ' + params.split(' ')[0] + ' warning on the ' + params.split(' ')[1] + ' axis')
+			#do something:
+
+	def sendCommand(self,command):
+		print 'Sending command: ' + command
+		if self.ser.is_open:
+			self.ser.write(command)
+		else:
+			raise Exception('Serial port not open')
+
+	#this should be opened as a thread once the port has been opened
+	def checkFeedback(self):
+		while True:
+			self.e.wait()
+			while self.ser.in_waiting > 0:
+				line = self.ser.readline()
+				self.parseFeedback(line)
 
 
--- Frontend/driver.py
+++ Frontend/driver.py
@@ -7,19 +7,8 @@ from controller import RotorController
 
 rc = RotorController()
 print rc.AZ_DEGREES_PER_TICK
-print rc.statusAZ
-print rc.statusEL
-rc.moveHome()
-print rc.statusAZ
-print rc.statusEL
 
 rc.move(90,30)
-print rc.statusAZ
-print rc.statusEL
 
-rc.parseCommand('HOME:EL')
-print rc.currentAZ
-rc.parseCommand('STEP:AZ')
-print rc.currentAZ
-rc.parseCommand('WARN:THERMAL AZ')
+
 
