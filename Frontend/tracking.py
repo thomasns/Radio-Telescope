d88f7cc302f6a7bc999f4fe409075006e9c4e88a
thomasns <ki4hbd.nathan@gmail.com>
2019-08-12 10:09:23 -0400

    Initial tracking code
    Added new tracking class and updated driver code to test it.
    Not 100% but some progress.
    
    Added docstrings to RotorController class.
    Updated class diagram.

---
+++ Frontend/tracking.py
@@ -0,0 +1,30 @@
+""" Controller.py 
+    this package serves as the link between the GUI front end and the hardware motor controller for the radio telescope. 
+
+    Nathan Thomas
+    1/17/19
+"""
+from skyfield.api import Topos, load
+import constant
+import threading
+
+
+class Tracking:
+
+	def __init__(self,target,location):
+		self.target = target
+		self.location = location
+		self.ts = load.timescale()
+
+
+	def setTarget(self, target):
+		self.target = target
+
+	def setLocation(self, location):
+		self.location = location
+
+	def calcAltAz(self):
+		t = self.ts.now()
+		apparent = self.location.at(t).observe(self.target).apparent().altaz()
+		print apparent
+		
